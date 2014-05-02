c4850d8a81771f0353c3192e8dc235961c249ad2
rodrigoahv@hotmail.com <rodrigoahv@hotmail.com@aef6b851-c686-90fc-faec-476fe9571423>
2014-05-02 04:41:34 +0000

    Estructura de navegacion de tablas Alumno y AsignacionProyecto

--- AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/AccesoActivity.java
+++ AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/AccesoActivity.java
@@ -1,8 +1,10 @@
 package sv.edu.ues.fia.appserviciosocial;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccesoActivity extends Activity {
 
@@ -11,12 +13,13 @@ public class AccesoActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_acceso);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.acceso, menu);
-		return true;
+	
+	public void ingresar(View v)
+	{
+		finish();
+		startActivity(new Intent(this, MenuTablasActivity.class));
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
 	}
+
 
 }
