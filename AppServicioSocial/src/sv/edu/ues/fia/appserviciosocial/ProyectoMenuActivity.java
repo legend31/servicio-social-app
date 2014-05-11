551f14ef6b9f33978f6dff03b066ce2ff4959d17
mcr.jblh@gmail.com <mcr.jblh@gmail.com@aef6b851-c686-90fc-faec-476fe9571423>
2014-05-11 17:50:41 +0000

    Problemas con NavigationDrawerActivity. Consiste en que agrego un intent a ese activity a una clase por ejemplo ProyectoMenuActivity pero al darle clic en el principal no me ejecuta ese activity

---
+++ AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/ProyectoMenuActivity.java
@@ -0,0 +1,26 @@
+package sv.edu.ues.fia.appserviciosocial;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class ProyectoMenuActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_proyecto_menu);
+			Intent intent = new Intent(this,NavigationDrawerActivity.class);
+			startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.proyecto_menu, menu);
+		return true;
+	}
+	
+}
