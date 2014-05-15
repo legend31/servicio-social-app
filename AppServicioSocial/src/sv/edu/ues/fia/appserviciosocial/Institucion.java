190176a49451c606a4d5fa569e28838727696003
danielperezsv@gmail.com <danielperezsv@gmail.com@aef6b851-c686-90fc-faec-476fe9571423>
2014-05-15 10:58:13 +0000

    GUI tabla institucion

--- AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/Institucion.java
+++ AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/Institucion.java
@@ -1,5 +1,36 @@
 package sv.edu.ues.fia.appserviciosocial;
 
 public class Institucion {
+	private String idInstitucion,
+				   nombre,
+				   nit;
+
+	public String getIdInstitucion() {
+		return idInstitucion;
+	}
+
+	public void setIdInstitucion(String idInstitucion) {
+		this.idInstitucion = idInstitucion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNit() {
+		return nit;
+	}
+
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+
+
+	
+	
 
 }
