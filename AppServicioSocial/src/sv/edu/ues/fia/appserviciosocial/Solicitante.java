3ad231d3142068c531bf254ada41ec0e629a5a42
danielperezsv@gmail.com <danielperezsv@gmail.com@aef6b851-c686-90fc-faec-476fe9571423>
2014-05-15 11:01:05 +0000

    GUI Solicitante

--- AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/Solicitante.java
+++ AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/Solicitante.java
@@ -1,5 +1,42 @@
 package sv.edu.ues.fia.appserviciosocial;
 
 public class Solicitante {
+	
+	private String idSolicitante,
+					nombre,
+					telefono,
+					correo;
 
+	public String getIdSolicitante() {
+		return idSolicitante;
+	}
+
+	public void setIdSolicitante(String idSolicitante) {
+		this.idSolicitante = idSolicitante;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
 }
