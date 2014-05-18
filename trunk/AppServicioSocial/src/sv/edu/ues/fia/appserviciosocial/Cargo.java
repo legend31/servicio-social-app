e9bee2e49ba06c09a516c1a94f3804eaba5ab0f1
danielperezsv@gmail.com <danielperezsv@gmail.com@aef6b851-c686-90fc-faec-476fe9571423>
2014-05-18 10:45:24 +0000

    Implementación en ControlDB

---
+++ trunk/AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/Cargo.java
@@ -0,0 +1,30 @@
+package sv.edu.ues.fia.appserviciosocial;
+
+public class Cargo {
+	private String idCargo,nombre,descripcion;
+
+	public String getIdCargo() {
+		return idCargo;
+	}
+
+	public void setIdCargo(String idCargo) {
+		this.idCargo = idCargo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
