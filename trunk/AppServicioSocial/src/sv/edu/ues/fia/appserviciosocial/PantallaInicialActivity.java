5bbd15ae857fff8bd8b260d0e85da2037af8ef0a
mcr.jblh@gmail.com <mcr.jblh@gmail.com@aef6b851-c686-90fc-faec-476fe9571423>
2014-05-22 16:17:39 +0000

    Interfaz y actividades de acuerdo al usuario que usa la app

--- trunk/AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/PantallaInicialActivity.java
+++ trunk/AppServicioSocial/src/sv/edu/ues/fia/appserviciosocial/PantallaInicialActivity.java
@@ -27,28 +27,30 @@ public class PantallaInicialActivity extends Activity implements
 	private String[] actividades = { "AlumnoMenuActivity",
 			"AsignacionProyectoMenuActivity", "BitacoraMenuActivity",
 			"CargoMenuActivity", "EncargadoMenuActivity",
-			"InstitucionMenuActivity", "SolicitanteMenuActivity",
+			"InstitucionMenuActivity", "InstitucionMenuActivity",
 			"ProyectoMenuActivity", "TipoProyectoMenuActivity",
 			"TipoTrabajoMenuActivity" };
 
+	private String[] actividadesAlumno = { "AlumnoMenuActivity",
+			"EncargadoMenuActivity", "InstitucionMenuActivity",
+			"ProyectoMenuActivity", "TipoProyectoMenuActivity" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listado);
 		Bundle b = getIntent().getExtras();
 		tipoUsuario = b.getInt("tipoUsuario");
 		ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
 
-		if(tipoUsuario==1)
-		{
+		if (tipoUsuario == 1) {
 			datos.add(new Lista_entrada(R.drawable.estudiante, "Alumno"));
 			datos.add(new Lista_entrada(R.drawable.aproyectos,
 					"Asignacion Proyectos"));
 			datos.add(new Lista_entrada(R.drawable.bitacora, "Bitacora"));
 			datos.add(new Lista_entrada(R.drawable.cargo, "Cargo"));
-			datos.add(new Lista_entrada(R.drawable.encargado,
-					"Encargado S.S"));
+			datos.add(new Lista_entrada(R.drawable.encargado, "Encargado S.S"));
 			datos.add(new Lista_entrada(R.drawable.institucion, "Institucion"));
 			datos.add(new Lista_entrada(R.drawable.solicitante, "Solicitante"));
 			datos.add(new Lista_entrada(R.drawable.proyecto, "Proyecto"));
@@ -76,97 +78,102 @@ public class PantallaInicialActivity extends Activity implements
 					}
 				}
 			});
-		}//fin if
-		else
-			if(tipoUsuario==2){
-				datos.add(new Lista_entrada(R.drawable.estudiante, "Alumno"));
-				datos.add(new Lista_entrada(R.drawable.encargado,
-						"Encargado S.S"));
-				datos.add(new Lista_entrada(R.drawable.institucion, "Institucion"));
-				datos.add(new Lista_entrada(R.drawable.proyecto, "Proyecto"));
-				datos.add(new Lista_entrada(R.drawable.proyectos, "Tipo Proyecto"));
-				lista = (GridView) findViewById(R.id.ListView_listado);
-				lista.setAdapter(new Lista_Adaptador(this, R.layout.entrada, datos) {
-					@Override
-					public void onEntrada(Object entrada, View view) {
-						if (entrada != null) {
-							TextView texto_superior_entrada = (TextView) view
-									.findViewById(R.id.textView_superior);
-							if (texto_superior_entrada != null)
-								texto_superior_entrada
-										.setText(((Lista_entrada) entrada)
-												.getTextoEncima());
+		}// fin if
+		else if (tipoUsuario == 2) {
+			datos.add(new Lista_entrada(R.drawable.estudiante, "Alumno"));
+			datos.add(new Lista_entrada(R.drawable.encargado, "Encargado S.S"));
+			datos.add(new Lista_entrada(R.drawable.institucion, "Institucion"));
+			datos.add(new Lista_entrada(R.drawable.proyecto, "Proyecto"));
+			datos.add(new Lista_entrada(R.drawable.proyectos, "Tipo Proyecto"));
+			lista = (GridView) findViewById(R.id.ListView_listado);
+			lista.setAdapter(new Lista_Adaptador(this, R.layout.entrada, datos) {
+				@Override
+				public void onEntrada(Object entrada, View view) {
+					if (entrada != null) {
+						TextView texto_superior_entrada = (TextView) view
+								.findViewById(R.id.textView_superior);
+						if (texto_superior_entrada != null)
+							texto_superior_entrada
+									.setText(((Lista_entrada) entrada)
+											.getTextoEncima());
 
-							ImageView imagen_entrada = (ImageView) view
-									.findViewById(R.id.imageView_imagen);
-							if (imagen_entrada != null)
-								imagen_entrada
-										.setImageResource(((Lista_entrada) entrada)
-												.getIdImagen());
-						}
+						ImageView imagen_entrada = (ImageView) view
+								.findViewById(R.id.imageView_imagen);
+						if (imagen_entrada != null)
+							imagen_entrada
+									.setImageResource(((Lista_entrada) entrada)
+											.getIdImagen());
 					}
-				});
-				
-			}
-		
-		
-		/*ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
-		datos.add(new Lista_entrada(R.drawable.estudiante, "Alumno"));
-		datos.add(new Lista_entrada(R.drawable.aproyectos,
-				"Asignacion Proyectos"));
-		datos.add(new Lista_entrada(R.drawable.bitacora, "Bitacora"));
-		datos.add(new Lista_entrada(R.drawable.cargo, "Cargo"));
-		datos.add(new Lista_entrada(R.drawable.encargado,
-				"Encargado S.S"));
-		datos.add(new Lista_entrada(R.drawable.institucion, "Institucion"));
-		datos.add(new Lista_entrada(R.drawable.solicitante, "Solicitante"));
-		datos.add(new Lista_entrada(R.drawable.proyecto, "Proyecto"));
-		datos.add(new Lista_entrada(R.drawable.proyectos, "Tipo Proyecto"));
-		datos.add(new Lista_entrada(R.drawable.trabajo, "Tipo Trabajo"));
-
-		lista = (GridView) findViewById(R.id.ListView_listado);
-		lista.setAdapter(new Lista_Adaptador(this, R.layout.entrada, datos) {
-			@Override
-			public void onEntrada(Object entrada, View view) {
-				if (entrada != null) {
-					TextView texto_superior_entrada = (TextView) view
-							.findViewById(R.id.textView_superior);
-					if (texto_superior_entrada != null)
-						texto_superior_entrada
-								.setText(((Lista_entrada) entrada)
-										.getTextoEncima());
-
-					ImageView imagen_entrada = (ImageView) view
-							.findViewById(R.id.imageView_imagen);
-					if (imagen_entrada != null)
-						imagen_entrada
-								.setImageResource(((Lista_entrada) entrada)
-										.getIdImagen());
 				}
-			}
-		});
-*/
+			});
+
+		}
+
+		/*
+		 * ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
+		 * datos.add(new Lista_entrada(R.drawable.estudiante, "Alumno"));
+		 * datos.add(new Lista_entrada(R.drawable.aproyectos,
+		 * "Asignacion Proyectos")); datos.add(new
+		 * Lista_entrada(R.drawable.bitacora, "Bitacora")); datos.add(new
+		 * Lista_entrada(R.drawable.cargo, "Cargo")); datos.add(new
+		 * Lista_entrada(R.drawable.encargado, "Encargado S.S")); datos.add(new
+		 * Lista_entrada(R.drawable.institucion, "Institucion")); datos.add(new
+		 * Lista_entrada(R.drawable.solicitante, "Solicitante")); datos.add(new
+		 * Lista_entrada(R.drawable.proyecto, "Proyecto")); datos.add(new
+		 * Lista_entrada(R.drawable.proyectos, "Tipo Proyecto")); datos.add(new
+		 * Lista_entrada(R.drawable.trabajo, "Tipo Trabajo"));
+		 * 
+		 * lista = (GridView) findViewById(R.id.ListView_listado);
+		 * lista.setAdapter(new Lista_Adaptador(this, R.layout.entrada, datos) {
+		 * 
+		 * @Override public void onEntrada(Object entrada, View view) { if
+		 * (entrada != null) { TextView texto_superior_entrada = (TextView) view
+		 * .findViewById(R.id.textView_superior); if (texto_superior_entrada !=
+		 * null) texto_superior_entrada .setText(((Lista_entrada) entrada)
+		 * .getTextoEncima());
+		 * 
+		 * ImageView imagen_entrada = (ImageView) view
+		 * .findViewById(R.id.imageView_imagen); if (imagen_entrada != null)
+		 * imagen_entrada .setImageResource(((Lista_entrada) entrada)
+		 * .getIdImagen()); } } });
+		 */
 
 		lista.setOnItemClickListener(this);
-		
-	}
 
+	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 		// TODO Auto-generated method stub
-		String nombreValues = actividades[position];
-		try {
-			Class<?> clase = Class.forName("sv.edu.ues.fia.appserviciosocial."
-					+ nombreValues);
-			Intent intent = new Intent(this, clase);
-			intent.putExtra("tipoUsuario", tipoUsuario);
-			startActivity(intent);
-			overridePendingTransition(R.anim.left_in, R.anim.left_out);
-			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		if (tipoUsuario == 1) {
+			String nombreValues = actividades[position];
+			try {
+				Class<?> clase = Class
+						.forName("sv.edu.ues.fia.appserviciosocial."
+								+ nombreValues);
+				Intent intent = new Intent(this, clase);
+				intent.putExtra("tipoUsuario", tipoUsuario);
+				startActivity(intent);
+				overridePendingTransition(R.anim.left_in, R.anim.left_out);
+
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else if (tipoUsuario == 2) {
+			String nombreValues = actividadesAlumno[position];
+			try {
+				Class<?> clase = Class
+						.forName("sv.edu.ues.fia.appserviciosocial."
+								+ nombreValues);
+				Intent intent = new Intent(this, clase);
+				intent.putExtra("tipoUsuario", tipoUsuario);
+				startActivity(intent);
+				overridePendingTransition(R.anim.left_in, R.anim.left_out);
+
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
